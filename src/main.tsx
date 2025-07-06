b54a2a8b79529ab796b1f7be54345593af6f7d9c
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2025-07-06 15:43:24 +0300

    feat: add amblify code

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '@aws-amplify/ui-react/styles.css';
+import './index.css'
+import App from './App.tsx'
 import { Authenticator } from "@aws-amplify/ui-react";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Authenticator>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+     <Authenticator>
       <App />
     </Authenticator>
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
